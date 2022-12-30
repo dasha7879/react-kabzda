8d67b373dd3df625267b56ac0d6c7bedec141fe8
dasha7879 <belka7879@gmail.com>
2022-12-31 02:51:09 +0300

    refactor: add components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,58 @@
 import React from 'react';
 import './App.css';
 
+function hello(){
+  debugger
+  return(
+    alert('Hello')
+  )
+}
+hello()
+
 function App() {
+  debugger
   return (
-    <div className="App">
-     Hello, samurai! Let'!
+    <div>
+     This is App
+     <Rating />
+     <Accordion />
     </div>
   );
+}
+
+function Rating() {
+  debugger
+  return (
+    <div>
+      <h3>Menu</h3>
+    <Star />
+    </div>
+  )
+}
+
+function Accordion(){
+  debugger
+  return(
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
+}
+
+function Star(){
+  return (
+    <div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+      <div>star</div>
+    </div>
+  )
 }
 
 export default App;
